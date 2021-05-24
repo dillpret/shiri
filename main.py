a877d3fa302c98e14440ffa398cd71a5e9c72af1
Dillon Pretorius <dillon.pretorius@vcontractor.co.za>
2021-05-24 18:53:15 +0200

    Spotify connecting

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 # main.py
 import os
-import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = os.getenv('COMMAND_PREFIX')
 
-bot = commands.Bot(command_prefix=PREFIX)
+SPOTIFY_ACCESS_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_SEARCH_URL = "https://api.spotify.com/v1/search"
+SPOTIFY_PLAYLIST_URL = "https://api.spotify.com/v1/playlists/4BvNLwSbqsrwtHXZ1erfAz/tracks"
+
+# SPOTIPY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+# SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
+bot = commands.Bot(command_prefix='>')
+
+
+@bot.command(name='hey')
+async def greet(ctx):
+    message = f'Hello, {ctx.author}, you sexy beast'
+    print(message)
+    await ctx.send(message)
+
+
+@bot.command(name='sh')
+async def share(ctx, args):
+    message = f'Received link: {args}'
+    print(message)
+    await ctx.send(message)
 
 
 @bot.event
@@ -19,13 +45,13 @@ async def on_ready():
         print(f' - {guild.name}:{guild.id}')
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content == 'some song':
-        await message.channel.send('Here I will send link')
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
+#
+#     if message.content == 'some song':
+#         await message.channel.send('Here I will send link')
 
 
 bot.run(TOKEN)
