9772515948ebd25fe7521565344d00cc38b01a45
Dillon Pretorius <dillon.pretorius@vcontractor.co.za>
2021-05-28 13:37:28 +0200

    Moved Deezer playlist URL to environment variable

--- deezer_integration.py
+++ deezer_integration.py
@@ -2,19 +2,13 @@ import requests
 
 DEEZER_SEARCH_URL = "https://api.deezer.com/search";
 
-# The real one
-# DEEZER_PLAYLIST_URL = "https://api.deezer.com/playlist/8866431842/tracks"
 
-# Test
-DEEZER_PLAYLIST_URL = "https://api.deezer.com/playlist/9104256582/tracks"
-
-
-def add_to_deezer_playlist(token, song_details):
+def add_to_deezer_playlist(token, song_details, playlist_url):
     search_params = {'access_token': token, 'q': song_details}
     search_response = requests.get(DEEZER_SEARCH_URL, params=search_params).json()
     song_id = search_response['data'][0]['id']
     song_link = search_response['data'][0]['link']
 
     add_params = {'access_token': token, 'songs': song_id, 'request_method': 'POST'}
-    requests.get(DEEZER_PLAYLIST_URL, params=add_params)
+    requests.get(playlist_url, params=add_params)
     return song_link
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DEEZER_TOKEN = os.getenv('DEEZER_TOKEN')
 PREFIX = os.getenv('COMMAND_PREFIX')
 SPOTIFY_PLAYLIST_URL = os.getenv('SPOTIFY_PLAYLIST_URL')
+DEEZER_PLAYLIST_URL = os.getenv('DEEZER_PLAYLIST_URL')
 
 bot = commands.Bot(command_prefix='>')
 
@@ -30,13 +31,13 @@ async def share(ctx, args):
             spotify_integration.add_to_spotify_playlist(args, SPOTIFY_PLAYLIST_URL)
 
             search_term = spotify_integration.get_song_search_term(args)
-            deezer_url = deezer_integration.add_to_deezer_playlist(DEEZER_TOKEN, search_term)
+            deezer_url = deezer_integration.add_to_deezer_playlist(DEEZER_TOKEN, search_term, DEEZER_PLAYLIST_URL)
             await ctx.send(f'Added {deezer_url} and the above Spotify link for \'{search_term}\'')
         else:
             spotify_url = spotify_integration.search_for_track_url(args)
             spotify_integration.add_to_spotify_playlist(spotify_url, SPOTIFY_PLAYLIST_URL)
 
-            deezer_url = deezer_integration.add_to_deezer_playlist(DEEZER_TOKEN, args)
+            deezer_url = deezer_integration.add_to_deezer_playlist(DEEZER_TOKEN, args, DEEZER_PLAYLIST_URL)
             await ctx.send(f'Added {spotify_url} and {deezer_url}')
     except Exception as e:
         await ctx.send(str(e))
